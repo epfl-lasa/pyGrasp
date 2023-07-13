b9ad498c91bafeff0dcc48986c4fc6fe88d84d7b
niederha <loic.niederhauser@epfl.ch>
2023-07-13 14:31:20 +0200

    remove previous venv before recreating one

--- setup_env.bash
+++ setup_env.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+rm -rf .venv
 python3.9 -m venv .venv          # Create venv (use any python version you like >=3.9)
 source .venv/bin/activate        # Activate venv
 pip install -r requirements.txt  # Install package requirements
-python -m build                  # Build package
-pip install -e .                 # Install package. -e for editable, developer mode. 
+python3.9 -m build               # Build package
+python3.9 -m pip install -e .    # Install package. -e for editable, developer mode. 
