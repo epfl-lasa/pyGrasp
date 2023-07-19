be26ee2e7447cf7e759e9e5ab4f29c0513901164
niederha <loic.niederhauser@epfl.ch>
2023-07-19 10:57:13 +0200

    Added a bit of verbosity to env setup

--- setup_env.bash
+++ setup_env.bash
@@ -27,11 +27,19 @@ done
 
 # Check if we need to remove past files and caches
 if [ "$rebuild"=true ] ; then
+    echo "Removing previous venv..."
     rm -rf .venv
+    echo "Done"
 fi
 
+echo "Creating venv..."
 python3.9 -m venv .venv          # Create venv (use any python version you like >=3.9)
+echo "Done"
+
+echo "Activating venv..."
 source .venv/bin/activate        # Activate venv
+echo "Done"
+
 pip install -r requirements.txt  # Install package requirements
 python3.9 -m build               # Build package
 python3.9 -m pip install -e .    # Install package. -e for editable, developer mode. 
